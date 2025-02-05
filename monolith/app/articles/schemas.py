5e6dbd66f3e4c53f9b4087cda04d6068661cd86e
Juan Vanegas <juan.vanegas@loka.com>
2025-02-04 20:28:55 -0500

    :bug: Fix datetime type

--- monolith/app/articles/schemas.py
+++ monolith/app/articles/schemas.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Annotated
 
 from pydantic import BaseModel, Field
@@ -8,12 +9,12 @@ from app.schemas import BaseAPIResponse
 class ArticleOut(BaseAPIResponse):
     title: str
     slug: str
-    created_at: Annotated[str, Field(serialization_alias="createdAt")]
-    updated_at: Annotated[str, Field(serialization_alias="updatedAt")]
+    created_at: Annotated[datetime, Field(serialization_alias="createdAt")]
+    updated_at: Annotated[datetime, Field(serialization_alias="updatedAt")]
     description: str
     tag_list: Annotated[list[str], Field(serialization_alias="tagList")]
     author: str
-    favorited: bool
+    favorited: bool  # TODO: verify if this is correct
     favorites_count: Annotated[int, Field(serialization_alias="favoritesCount")]
 
 
